d363ae2626b245f807cc51faa43f603a4f48eb2a
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2015-04-07 09:17:41 +0900

    experiment

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -22,6 +22,48 @@ angular.module('trickerApp.controllers', []).
         title: 'キッカーを使ってFsシフティ IKENOCITY PARK ACADEMY　スノーボードHow to'
       };
       $scope.world = 'world';
+
+      var trickList = [
+        {
+          type: 'category',
+          name: 'ストレートジャンプ',
+          tricks: [
+            {
+              type: 'item',
+              name: 'ollie',
+              name_ja: 'オーリー'
+            },
+            {
+              type: 'item',
+              name: 'nollie',
+              name_ja: 'ノーリー'
+            },
+            {
+              type: 'item',
+              name: 'shifty',
+              name_ja: 'シフティ'
+            }
+          ]
+        },
+        {
+          type: 'category',
+          name: 'キッカー',
+          tricks: [
+            {
+              type: 'item',
+              name: 'frontside-triple-cork-1440',
+              name_ja: 'フロントサイドトリプルコーク1440'
+            },
+            {
+              type: 'item',
+              name: 'backside-triple-cork-1440',
+              name_ja: 'バックサイドトリプルコーク1440'
+            }
+          ]
+        }
+      ];
+
+      $scope.rows = trickList;
     }
   ]).
   controller('VideoListCtrl', ['$scope', '$routeParams', '$http',
