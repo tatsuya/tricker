8b19510df0b99ede53cbe39b719088e393e37490
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2015-04-08 09:08:14 +0900

    store videos to file

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 var tree = require('../data/tree');
 var tricks = require('../data/tricks');
-var videos = require('../data/videos');
+var videos = require('../lib/videos');
 
 router.get('/tricks', function(req, res) {
   res.send(tree);
@@ -16,9 +16,13 @@ router.get('/tricks/:trick', function(req, res) {
   res.send(tricks[trick]);
 });
 
+router.get('/videos', function(req, res) {
+  res.send(videos.list());
+});
+
 router.get('/videos/:trick', function(req, res) {
   var trick = req.params.trick;
-  res.send(videos[trick]);
+  res.send(videos.filterByTag(trick));
 });
 
 module.exports = router;
